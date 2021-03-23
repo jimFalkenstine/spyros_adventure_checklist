e796689cb8c5b85731219761343cd5d9226e77a1
Jim Falkenstine <jim.fakenstine@gmail.com>
2021-03-22 22:06:45 -0400

    added index values to characters.js.  Finalized interactivity of checkboxes with JavaScript.  Tweaked menu to point to section headers.

--- js/characters.js
+++ js/characters.js
@@ -1,230 +1,278 @@
 const coreSkylanders = [
     {
         name: 'Bash',
-        image: 'images/bash.jpg'   
+        image: 'images/bash.jpg',
+        index: 0
     },
     {
 		name: 'Boomer',
-		image: 'images/boomer.jpg'
+		image: 'images/boomer.jpg',
+        index: 1
 	},
     {
 		name: 'Camo',
-		image: 'images/camo.jpg'
+		image: 'images/camo.jpg',
+        index: 2
 	},
     {
 		name: 'Chop Chop',
-		image: 'images/chop_chop.jpg'
+		image: 'images/chop_chop.jpg',
+        index: 3
 	},
     {
 		name: 'Cynder',
-		image: 'images/cynder.jpg'
+		image: 'images/cynder.jpg',
+        index: 4
 	},
     {
 		name: 'Dino-Rang',
-		image: 'images/dino-rang.jpg'
+		image: 'images/dino-rang.jpg',
+        index: 5
 	},
     {
 		name: 'Double Trouble',
-		image: 'images/double_trouble.jpg'
+		image: 'images/double_trouble.jpg',
+        index: 6
 	},
     {
 		name: 'Drill Sergeant',
-		image: 'images/drill_sergeant.jpg'
+		image: 'images/drill_sergeant.jpg',
+        index: 7
 	},
     {
 		name: 'Drobot',
-		image: 'images/drobot.jpg'
+		image: 'images/drobot.jpg',
+        index: 8
 	},
     {
 		name: 'Eruptor',
-		image: 'images/eruptor.jpg'
+		image: 'images/eruptor.jpg',
+        index: 9
 	},
     {
 		name: 'Flameslinger',
-		image: 'images/flameslinger.jpg'
+		image: 'images/flameslinger.jpg',
+        index: 10
 	},
     {
 		name: 'Ghost Roaster',
-		image: 'images/ghost_roaster.jpg'
+		image: 'images/ghost_roaster.jpg',
+        index: 11
 	},
     {
 		name: 'Gill Grunt',
-		image: 'images/gill_grunt.jpg'
+		image: 'images/gill_grunt.jpg',
+        index: 12
 	},
     {
 		name: 'Hex',
-		image: 'images/hex.jpg'
+		image: 'images/hex.jpg',
+        index: 13,
 	},
     {
 		name: 'Ignitor',
-		image: 'images/ignitor.jpg'
+		image: 'images/ignitor.jpg',
+        index: 14
 	},
     {
 		name: 'Lightning Rod',
-		image: 'images/lightning_rod.jpg'
+		image: 'images/lightning_rod.jpg',
+        index: 15
 	},
     {
 		name: 'Prism Break',
-		image: 'images/prism_break.jpg'
+		image: 'images/prism_break.jpg',
+        index: 16
 	},
     {
 		name: 'Slam Bam',
-		image: 'images/slam_bam.jpg'
+		image: 'images/slam_bam.jpg',
+        index: 17
 	},
     {
 		name: 'Sonic Boom',
-		image: 'images/sonic_boom.jpg'
+		image: 'images/sonic_boom.jpg',
+        index: 18
 	},
     {
 		name: 'Spyro',
-		image: 'images/spyro.jpg.'
+		image: 'images/spyro.jpg.',
+        index: 19
 	},
     {
 		name: 'Stealth Elf',
-		image: 'images/stealth_elf.jpg'
+		image: 'images/stealth_elf.jpg',
+        index: 20
 	},
     {
 		name: 'Stump Smash',
-		image: 'images/stump_smash.jpg'
+		image: 'images/stump_smash.jpg',
+        index: 21
 	},
     {
 		name: 'Sunburn',
-		image: 'images/sunburn.jpg'
+		image: 'images/sunburn.jpg',
+        index: 22
 	},
     {
 		name: 'Terrafin',
-		image: 'images/terrafin.jpg'
+		image: 'images/terrafin.jpg',
+        index: 23
 	},
     {
 		name: 'Trigger Happy',
-		image: 'images/trigger_happy.jpg'
+		image: 'images/trigger_happy.jpg',
+        index: 24
 	},
     {
 		name: 'Voodood',
-		image: 'images/voodood.jpg'
+		image: 'images/voodood.jpg',
+        index: 25
 	},
     {
 		name: 'Warnado',
-		image: 'images/warnado.jpg'
+		image: 'images/warnado.jpg',
+        index: 26
 	},
     {
 		name: 'Wham-Shell',
-		image: 'images/wham-shell.jpg'
+		image: 'images/wham-shell.jpg',
+        index: 27
 	},
     {
 		name: 'Whirlwind',
-		image: 'images/whirlwind.jpg'
+		image: 'images/whirlwind.jpg',
+        index: 28
 	},
     {
 		name: 'Wrecking Ball',
-		image: 'images/wrecking_ball.jpg'
+		image: 'images/wrecking_ball.jpg',
+        index: 29
 	},
     {
 		name: 'Zap',
-		image: 'images/zap.jpg'
+		image: 'images/zap.jpg',
+        index: 30
 	},
     {
 		name: 'Zook',
-		image: 'images/zook.jpg'
+		image: 'images/zook.jpg',
+        index: 31
 	}
 ]
 
 const inGameVariants = [
     {
         name: 'Dark Spyro',
-        image: 'images/dark_spyro.jpg'
+        image: 'images/dark_spyro.jpg',
+        index: 32
     },
 	{
         name: 'Legendary Bash',
-        image: 'images/legendary_bash.jpg'
+        image: 'images/legendary_bash.jpg',
+        index: 33
     },
 	{
         name: 'Legendary Chop Chop',
-        image: 'images/legendary_chop_chop.jpg'
+        image: 'images/legendary_chop_chop.jpg',
+        index: 34
     },
 	{
         name: 'Legendary Spyro',
-        element: 'Magic',
-        image: 'images/legendary_spyro.jpg'
+        image: 'images/legendary_spyro.jpg',
+        index: 35
     },
 	{
         name: 'Legendary Trigger Happy',
-        element: 'Tech',
-        image: 'images/legendary_trigger_happy.jpg'
+        image: 'images/legendary_trigger_happy.jpg',
+        index: 36
     },
 	{
         name: 'Sidekick Gill Runt',
-        element: 'Water',
-        image: 'images/gill_runt.jpg'
+        image: 'images/gill_runt.jpg',
+        index: 37
     },
     {
         name: 'Sidekick Terrabite',
-        element: 'Earth',
-        image: 'images/terrabite.jpg'
+        image: 'images/terrabite.jpg',
+        index: 38
     },
     {
         name: 'Sidekick Trigger Snappy',
-        element: 'Tech',
-        image: 'images/trigger_snappy.jpg'
+        image: 'images/trigger_snappy.jpg',
+        index: 39
     },
     {
         name: 'Sidekick Whisper Elf',
-        element: 'Life',
-        image: 'images/whisper_elf.jpg'
+        image: 'images/whisper_elf.jpg',
+        index: 40
     },
 ]
 
 const magicItems = [
     {
         name: 'Darklight Crypt',
-        image: 'images/darklight_crypt.jpg'
+        image: 'images/darklight_crypt.jpg',
+        index: 41
     },
     {
         name: 'Healing Elixir',
-        image: 'images/healing_elixir.jpg'
+        image: 'images/healing_elixir.jpg',
+        index: 42
     },
     {
         name: 'Time Twister',
-        image: 'images/time_twister.jpg'
+        image: 'images/time_twister.jpg',
+        index: 43
     },
     {
         name: "Dragon's Peak", 
-        image: 'images/dragons_peak.jpg'
+        image: 'images/dragons_peak.jpg',
+        index: 44
     },
     {
         name: 'Winged Boots',
-        image: 'images/winged_boots.jpg'
+        image: 'images/winged_boots.jpg',
+        index: 45
     },
     {
         name: 'Sparx Dragonfly',
-        image: 'images/sparx_dragonfly.jpg'
+        image: 'images/sparx_dragonfly.jpg',
+        index: 46
     },
     {
         name: 'Empire of Ice',
-        image: 'images/empire_of_ice.jpg'
+        image: 'images/empire_of_ice.jpg',
+        index: 47
     },
     {
         name: 'Anvil Rain',
-        image: 'images/anvil_rain.jpg'
+        image: 'images/anvil_rain.jpg',
+        index: 48
     },
     {
         name: 'Sky-Iron Shield',
-        image: 'images/sky-iron_shield.jpg'
+        image: 'images/sky-iron_shield.jpg',
+        index: 49
     },
     {
         name: 'Pirate Seas',
-        image: 'images/pirate_seas.jpg'
+        image: 'images/pirate_seas.jpg',
+        index: 50
     },
     {
         name: 'Hidden Treasure',
-        image: 'images/hidden_treasure.jpg'
+        image: 'images/hidden_treasure.jpg',
+        index: 51
     },
     {
         name: 'Ghost Swords',
-        image: 'images/ghost_swords.jpg'
+        image: 'images/ghost_swords.jpg',
+        index: 52
     },
     {
         name: 'Volcanic Vault',
-        image: 'images/volcanic_vault.jpg'
+        image: 'images/volcanic_vault.jpg',
+        index: 53
     }
 ]
--- js/new_script.js
+++ js/new_script.js
@@ -3,15 +3,16 @@ function populateCoreSkylanders () {
   const main = document.querySelector('#core_skylanders');
   let html = '';
   
-  for ( let i = 0; i < coreSkylanders.length; i++) {
+  for (let i = 0; i < coreSkylanders.length; i++) {
     let character = coreSkylanders[i];
     html += `  
-        <div class='figure'>
+        <div class = "figure" id="${character.name}_div"}>
           <img src="${character.image}" alt="${character.name}">
           <form>
-            <input type="checkbox" id="${character.name}" name="${character.name}" value="${character.name}">
+            <input type="checkbox" id="${character.name}" name="${character.name}" value="${character.name}" onclick="myFunction(${character.index})">
             <label for="${character.name}">${character.name}</label>
           </form>
+          <p class="figure_text" style="display:none">You have ${character.name}!</p>
         </div>
     `;
   }
@@ -23,15 +24,16 @@ function populateInGameVariants () {
   const main = document.querySelector('#in-game_variants');
   let html = '';
   
-  for ( let i = 0; i < inGameVariants.length; i++) {
+  for (let i = 0; i < inGameVariants.length; i++) {
     let character = inGameVariants[i];
     html += `
       <div class='figure'>
       <img src="${character.image}" alt="${character.name}">
       <form>
-            <input type="checkbox" id="${character.name}" name="${character.name}" value="${character.name}">
+            <input type="checkbox" id="${character.name}" name="${character.name}" value="${character.name}" onclick="myFunction(${character.index})">
             <label for="${character.name}">${character.name}</label>
           </form>
+          <p class="figure_text" style="display:none">You have ${character.name}!</p>
       </div>
     `;
   }
@@ -43,20 +45,37 @@ function populateMagicItems () {
   const main = document.querySelector('#magic_items');
   let html = '';
   
-  for ( let i = 0; i < magicItems.length; i++) {
+  for (let i = 0; i < magicItems.length; i++) {
     let character = magicItems[i];
     html += `
       <div class='figure'>
       <img src="${character.image}" alt="${character.name}">
       <form>
-            <input type="checkbox" id="${character.name}" name="${character.name}" value="${character.name}">
+            <input type="checkbox" id="${character.name}" name="${character.name}" value="${character.name}" onclick="myFunction(${character.index})">
             <label for="${character.name}">${character.name}</label>
           </form>
+          <p class="figure_text" style="display:none">You have ${character.name}!</p>
       </div>
     `;
   }
   
   main.insertAdjacentHTML('beforeend', html);
+}
+
+function myFunction (index) {
+  const checkBox = document.querySelectorAll('input')[index];
+  const text = document.querySelectorAll('.figure_text')[index];
+  const figure = document.querySelectorAll('.figure')[index];
+
+  if (checkBox.checked == true) {
+    text.style.display = "block";
+    figure.style.opacity= "1";
+    figure.style.fontWeight = "bold";
+  } else {
+    text.style.display ="none";
+    figure.style.opacity = "0.5";
+    figure.style.fontWeight = "normal";
+  }
 }
 
 populateCoreSkylanders();
